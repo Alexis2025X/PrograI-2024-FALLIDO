d32582fce8da5ab892cd8245af777b0a279c5919
Javier Romero <usis037621ugb.edu.sv>
2024-07-24 09:49:33 -0600

    Ejercicio obtener el promedio de una serie de numeros

--- Mi_Primer_Proyecto/Mi_Primer_Proyecto/Program.cs
+++ Mi_Primer_Proyecto/Mi_Primer_Proyecto/Program.cs
@@ -10,59 +10,14 @@ namespace Mi_Primer_Proyecto
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Computo I");
-
-            Console.Write("lab1: ");
-            double lab1 = double.Parse(Console.ReadLine());
-
-            Console.Write("lab2: ");
-            double lab2 = double.Parse(Console.ReadLine());
-
-            Console.Write("parcial1: ");
-            double parcial1 = double.Parse(Console.ReadLine());
-
-            double c1 = lab1 * 30 / 100 + lab2 * 30 / 100 + parcial1 * 40 / 100;
-            Console.WriteLine("La nota de C1 es: {0}", c1);
-
-            Console.WriteLine("//////////////////////////////");
-            Console.WriteLine("Computo II");
-
-            Console.Write("lab1: ");
-            double lab1II = double.Parse(Console.ReadLine());
-
-            Console.Write("lab2: ");
-            double lab2II = double.Parse(Console.ReadLine());
-
-            Console.Write("parcial2: ");
-            double parcial2 = double.Parse(Console.ReadLine());
-
-            double c2 = lab1II * 30 / 100 + lab2II * 30 / 100 + parcial2 * 40 / 100;
-            Console.WriteLine("La nota de C2 es: {0}", c2);
-
-            Console.WriteLine("//////////////////////////////");
-            Console.WriteLine("Computo III");
-
-            Console.Write("lab1: ");
-            double lab1III = double.Parse(Console.ReadLine());
-
-            Console.Write("lab2: ");
-            double lab2III = double.Parse(Console.ReadLine());
-
-            Console.Write("parcial3: ");
-            double parcial3 = double.Parse(Console.ReadLine());
-
-            double c3 = lab1III * 30 / 100 + lab2III * 30 / 100 + parcial3 * 40 / 100;
-            Console.WriteLine("La nota de C3 es: {0}", c3);
-
-            //double Notafinal = (c1 + c2 + c3)/3 ;
-            double Notafinal = Math.Round(((c1 + c2 + c3) / 3),1);
-
-            Console.WriteLine("La nota final de la asignatura es: {0}", Notafinal);
-            Console.WriteLine("//////////////////////////////");
-            if (Notafinal < 6)
-                Console.WriteLine("Usted a reprobado");
-            else
-                Console.WriteLine("Felicidades usted a aprobado");
+            //Ejercicio obtener el promedio de una serie de numeros
+            int[] serie = new int[] { 5, 4, 6, 8, 9 }; //matriz, esta se indica con []
+            int suma = 0;
+            foreach (int num in serie){
+                suma = suma + num; //suma y asignación, tambien se puede hacer así: suma += num;
+            }
+            decimal prom = (suma / serie.Length);
+            Console.WriteLine("La suma es: {0}, el promedio {1}", suma, prom); //m nos ayuda  a establecer decimales, Length da la logitud de una matriz
 
             //Hace una pausa en la consola lo cual nos permite leer lo que se imprime
             Console.ReadLine();
